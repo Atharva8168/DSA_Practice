e80c114633fcaddac7fe344062f234cd0b5ab79e
Atharva Bauskar <bauskaratharva@gmail.com>
2021-12-18 16:17:59 +0530

    HashTable

---
+++ src/HashTable/HashTable.java
@@ -0,0 +1,115 @@
+package HashTable;
+
+import java.util.LinkedList;
+
+public class HashTable {
+
+    private class Entry{
+        private int key;
+        private String value;
+
+        public Entry(int key , String value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private LinkedList<Entry>[] entries = new LinkedList[5];
+    public void put(int key , String value){
+
+        var entry = getEntry(key);
+        if (entry != null) {
+            entry.value = value;
+            return;
+        }
+        var bucket = getOrCreateBucket(key);
+        bucket.add(new Entry(key, value));
+
+
+//        var bucket = getBucket(key);
+//        if (entries[index] == null){
+//            entries[index] = new LinkedList<>();
+//        }
+//
+//        for (var entry : bucket){
+//            if (entry.key == key) {
+//                entry.value = value;
+//                return;
+//            }
+//        }
+
+//        var entry = new Entry(key , value);
+//        bucket.addLast(entry);
+    }
+
+
+    public String get(int key){
+        var entry = getEntry(key);
+        return (entry == null) ? null : entry.value;
+
+
+//        var bucket = getBucket(key);
+//        if (bucket != null) {
+//            for (var entry : bucket)
+//                if (entry.key == key) {
+//                    return entry.value;
+//                }
+//        }
+//        return null;
+    }
+
+
+    public void remove(int key){
+        var entry = getEntry(key);
+        if (entry == null) {
+            throw new IllegalStateException();
+        }
+        getBucket(key).remove(entry);
+
+//        var index = hash(key);
+//        var bucket = entries[index];
+//        if (bucket == null)
+//            throw new IllegalStateException();
+//        for (var entry : bucket) {
+//            if (entry.key == key) {
+//                bucket.remove(entry);
+//                return;
+//            }
+//        }
+//        throw new IllegalStateException();
+    }
+
+
+    private Entry getEntry(int key){
+        var bucket = getBucket(key);
+        if (bucket != null) {
+            for (var entry : bucket){
+                if (entry.key == key) {
+                    return entry;
+                }
+            }
+        }
+        return null;
+    }
+
+    private LinkedList<Entry> getBucket(int key){
+        return entries[hash(key)];
+    }
+
+    private LinkedList<Entry> getOrCreateBucket(int key)
+    {
+        var index = hash(key);
+        var bucket = entries[index];
+        if (bucket == null)
+            entries[index] = new LinkedList<Entry>();
+        return bucket;
+    }
+
+    private int hash(int key){
+        return key % entries.length;
+    }
+
+
+
+
+}
--- src/HashTable/main.java
+++ src/HashTable/main.java
@@ -2,11 +2,23 @@ package HashTable;
 
 public class main {
     public static void main(String[] args) {
-        CharFinder ab = new CharFinder();
-        var nonRepeated = ab.firstNonRepeatingChar("a green apple");
-        System.out.println(nonRepeated);
-        var repeated = ab.firstRepeatedCharacter("a green apple");
-        System.out.println(repeated);
+        //  CharFinder
+//        CharFinder ab = new CharFinder();
+//        var nonRepeated = ab.firstNonRepeatingChar("a green apple");
+//        System.out.println(nonRepeated);
+//        var repeated = ab.firstRepeatedCharacter("a green apple");
+//        System.out.println(repeated);
+
+
+        // HashTable
+        HashTable table = new HashTable();
+        table.put(6 , "A");
+        table.put(8 , "B");
+        table.put(11 , "C");
+        table.put(6 , "C");
+        table.remove(8);
+        table.remove(11);
+        System.out.println(table.get(11));
 
     }
 }
