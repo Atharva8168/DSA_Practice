3ce21bb2c3990a5787d11eaea7e546bf58f748b4
Atharva Bauskar <bauskaratharva@gmail.com>
2022-01-18 16:44:34 +0530

    Heap removing

--- src/com/company/Heap/Heap.java
+++ src/com/company/Heap/Heap.java
@@ -13,6 +13,78 @@ public class Heap {
          bubbleUp();
     }
 
+    public int remove(){
+        if(isEmpty())
+            throw new IllegalStateException();
+
+        var root = items[0];
+        items[0] = items[--size];
+
+        bubbleDown();
+
+        return root;
+    }
+
+    private void bubbleDown(){
+        var index = 0;
+        while(index <= size && !isValidParent(index)) {
+            var largerChildIndex = largerChildIndex(index);
+            swap(largerChildIndex , index);
+            index = largerChildIndex;
+        }
+    }
+
+    private boolean hasLeftChild(int index){
+        return leftChildIndex(index) <= size;
+    }
+
+    private boolean hasRightChild(int index){
+        return rightChildIndex(index) <= size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    private int largerChildIndex(int index){
+        if (!hasLeftChild(index))
+            return index;
+
+        if (!hasRightChild(index))
+            return leftChildIndex(index);
+
+        return (leftChild(index) > rightChild(index) ?
+                leftChildIndex(index) : rightChildIndex(index));
+    }
+
+    private boolean isValidParent(int index){
+        if (!hasLeftChild(index))
+            return true;
+
+        var isValid = items[index] >= leftChild(index);
+
+        if (!hasRightChild(index))
+            isValid &= items[index] >= rightChild(index);
+
+        return isValid;
+    }
+
+    private int leftChild(int index){
+        return items[leftChildIndex(index)];
+    }
+
+    private int rightChild(int index){
+        return items[rightChildIndex(index)];
+    }
+
+    private int leftChildIndex(int index){
+        return index * 2 + 1;
+    }
+
+    private int rightChildIndex(int index){
+        return index * 2 + 2;
+    }
+
     public boolean isFull(){
         return size == items.length;
     }
--- src/com/company/Heap/main.java
+++ src/com/company/Heap/main.java
@@ -6,8 +6,10 @@ public class main {
         heap.insert(10);
         heap.insert(5);
         heap.insert(17);
-        heap.insert(22);
         heap.insert(4);
+        heap.insert(22);
+        heap.remove();
         System.out.println("Done");
     }
 }
+
