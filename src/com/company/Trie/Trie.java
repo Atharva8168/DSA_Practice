931cdf1b1dd54cbac919de3ecd91c31f776b07d1
Atharva Bauskar <bauskaratharva@gmail.com>
2022-01-21 17:01:11 +0530

    Trie Inserting using HashMap

--- src/com/company/Trie/Trie.java
+++ src/com/company/Trie/Trie.java
@@ -1,11 +1,16 @@
 package com.company.Trie;
 
+import java.util.HashMap;
+
 public class Trie {
     public static int ALPHABET_SIZE = 26;
 
     private class Node {
         private char value;
-        private Node[] children = new Node[ALPHABET_SIZE];
+        // Using array
+//        private Node[] children = new Node[ALPHABET_SIZE];
+        // Using HashMap -> to saw memory
+        private HashMap<Character, Node> children = new HashMap<>();
         private boolean isEndOfWord;
 
         public Node(char value) {
@@ -24,11 +29,17 @@ public class Trie {
         var current = root;
 
         for (var alphabet : word.toCharArray()){
-            var index = alphabet - 'a';
+            // Using array
+//            var index = alphabet - 'a';
 
-            if (current.children[index] == null)
-                current.children[index] = new Node(alphabet);
-            current  =  current.children[index];
+//            if (current.children[index] == null)
+//                current.children[index] = new Node(alphabet);
+//            current  =  current.children[index];
+
+            // Using HashMap
+            if (current.children.get(alphabet) == null)
+                current.children.put(alphabet, new Node(alphabet));
+            current = current.children.get(alphabet);
         }
         current.isEndOfWord = true;
     }
