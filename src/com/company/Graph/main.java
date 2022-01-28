7e4a4c69c1dc75255308fe26acff6d6fd3a0d3e0
Atharva Bauskar <bauskaratharva@gmail.com>
2022-01-28 16:55:40 +0530

    Graph Topological Sort'

--- src/com/company/Graph/main.java
+++ src/com/company/Graph/main.java
@@ -3,19 +3,30 @@ package com.company.Graph;
 public class main {
     public static void main(String[] args) {
         Graph graph = new Graph();
+//        graph.addNode("A");
+//        graph.addNode("B");
+//        graph.addNode("C");
+//        graph.addNode("D");
+//        graph.addEdge("A","B");
+//        graph.addEdge("B","D");
+//        graph.addEdge("D","C");
+//        graph.addEdge("A","C");
+////        graph.removeNode("B");
+////        graph.removeEdge("A","C");
+////        graph.print();
+////        graph.traverseDepthFirstRec("A");
+////        graph.traverseDepthFirst("B");
+//        graph.traverseBreathFirst("K");
+
+        graph.addNode("X");
         graph.addNode("A");
         graph.addNode("B");
-        graph.addNode("C");
-        graph.addNode("D");
-        graph.addEdge("A","B");
-        graph.addEdge("B","D");
-        graph.addEdge("D","C");
-        graph.addEdge("A","C");
-//        graph.removeNode("B");
-//        graph.removeEdge("A","C");
-//        graph.print();
-//        graph.traverseDepthFirstRec("A");
-//        graph.traverseDepthFirst("B");
-        graph.traverseBreathFirst("K");
+        graph.addNode("P");
+        graph.addEdge("X", "A");
+        graph.addEdge("X", "B");
+        graph.addEdge("A", "P");
+        graph.addEdge("B", "P");
+        System.out.println(graph.topologicalSort());;
+
     }
 }
